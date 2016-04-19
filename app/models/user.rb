051b6734b58c017f7dabb4d43eca1d8ad4b13a04
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2016-04-19 05:17:22 +0000

    added username to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+
+  has_many :places
+
+  attr_accessor :login
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
 end
