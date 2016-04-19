050cad5834991c92f9a5fad276a4a74de657c91a
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2016-04-19 06:02:10 +0000

    fixed username bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,26 @@
 class User < ActiveRecord::Base
+	attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
 
   has_many :places
 
-  attr_accessor :login
+ 
 
   validates :username,
   :presence => true,
   :uniqueness => {
     :case_sensitive => false
   }
+
+    def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
 end
