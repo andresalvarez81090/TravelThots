7a532800c4e2b38c6688da638c0007115015f357
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2016-04-23 01:43:09 +0000

    styled error field

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,8 +9,12 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, status: :unprocessable_entity
+		end
 	end
 
 	def show
@@ -27,11 +31,17 @@ class PlacesController < ApplicationController
 
 	def update
 		@place = Place.find(params[:id])
+
 		if @place.user != current_user
 			return render text: 'Not Allowed', status: :forbidden
 		end
+
 		@place.update_attributes(place_params)
-		redirect_to place_path(@place)
+		if @place.valid?
+			redirect_to place_path(@place)
+		else
+			render :edit, status: :unprocessable_entity
+		end
 	end
 
 	def destroy
@@ -39,7 +49,7 @@ class PlacesController < ApplicationController
 		if @place.user != current_user
 			return render text: 'Not Allowed', status: :forbidden
 		end
-		
+
 		@place.destroy
 		redirect_to root_path
 	end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,7 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	validates :city, presence: true
+	validates :country, presence: true
+	validates :rating, presence: true
+	validates :tips, presence: true
 end
